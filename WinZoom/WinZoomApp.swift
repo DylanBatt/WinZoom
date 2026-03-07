a66a0311dae70887ae5c60c8fa56b1346658c983
dylanbatt <info@dylanbatt.com>
2026-03-07 20:24:44 +0000

    Initial Commit

---
+++ WinZoom/WinZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinZoomApp.swift
+//  WinZoom
+//
+//  Created by Dylan Batt on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WinZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
